a7a92ee28850c678f35353cadab063c5288b3300
Pankaj Kumar <pk110495@gmail.com>
2022-02-06 17:35:56 +0530

    added name regex program

---
+++ NameRegex.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexPattern
+{
+    class Patterns
+    {
+      public static string regex2 = "^[A-Z][a-z]{2,}$";
+      public bool validateFirstName(string firstName)
+        {
+            return Regex.IsMatch(firstName, regex2);
+        }
+     }
